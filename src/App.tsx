450eb71533d0b127abf41f5e10678a390fb5c775
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-28 03:52:03 +0700

    app: wrapped component with the ApolloProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import Landing from './components/pages/landing';
 import PokemonSearch from './components/pages/pokemon-search/view';
+import apolloClient from './core/apollo'
 
 function App() {
-  return <Router>
-    <Switch>
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/pokemon-search" component={PokemonSearch}/>
-    </Switch>
-  </Router>
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/pokemon-search" component={PokemonSearch}/>
+        </Switch>
+      </Router>
+    </ApolloProvider>
+  )
 }
 
 export default App;
