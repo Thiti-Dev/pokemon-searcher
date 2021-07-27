38a7e4cbc668a9d4e729a7ad3af116e0cf3d51f6
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-27 23:36:45 +0700

    :lipstick: created empty (pokemon-search)component

---
+++ src/components/pages/pokemon-search/view/PokemonSearch.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokemonSearch:React.FC<any> = () => {
+  return (
+    <div>
+      <h1>Search mainstream</h1>
+    </div>
+  )
+}
+
+export default PokemonSearch
