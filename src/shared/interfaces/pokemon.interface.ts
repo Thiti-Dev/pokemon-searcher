79c2db56cc269c2d5971d3ce33a3b419186ede68
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-28 00:27:47 +0700

    shared->interface: created IPokemon definition

---
+++ src/shared/interfaces/pokemon.interface.ts
@@ -0,0 +1,97 @@
+export interface IPokemon {
+    number: 'string';
+    name: 'string';
+    weight: {
+        minimum: 'string';
+        maximum: 'string';
+    };
+    height: {
+        minimum: 'string';
+        maximum: 'string';
+    };
+    classification: 'string';
+    types: ['string'];
+    resistant: ['string'];
+    attacks: {
+        fast: [
+            {
+                name: 'string';
+                type: 'string';
+                damage: 'number';
+            },
+        ];
+        special: [
+            {
+                name: 'string';
+                type: 'string';
+                damage: 'number';
+            },
+        ];
+    };
+    weaknesses: ['string'];
+    fleeRate: 'number';
+    maxCP: 'number';
+    evolutions: [
+        {
+            number: 'string';
+            name: 'string';
+            weight: {
+                minimum: 'string';
+                maximum: 'string';
+            };
+            height: {
+                minimum: 'string';
+                maximum: 'string';
+            };
+            classification: 'string';
+            types: ['string'];
+            resistant: ['string'];
+            attacks: {
+                fast: [
+                    {
+                        name: 'string';
+                        type: 'string';
+                        damage: 'number';
+                    },
+                ];
+                special: [
+                    {
+                        name: 'string';
+                        type: 'string';
+                        damage: 'number';
+                    },
+                ];
+            };
+            weaknesses: ['string'];
+            fleeRate: 'number';
+            maxCP: 'number';
+            evolutions: [
+                {
+                    number: 'string';
+                    name: 'string';
+                    weight: {
+                        minimum: 'string';
+                        maximum: 'string';
+                    };
+                    height: {
+                        minimum: 'string';
+                        maximum: 'string';
+                    };
+                    classification: 'string';
+                    types: ['string'];
+                    resistant: ['string'];
+                    attacks: {
+                        fast: [
+                            {
+                                name: 'string';
+                                type: 'string';
+                                damage: 'number';
+                            },
+                        ];
+                        special: [null];
+                    };
+                },
+            ];
+        },
+    ];
+}
