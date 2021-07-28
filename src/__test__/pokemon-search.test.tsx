0d94c5872864349c587fff5c2ad6c9b22d2d77e1
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-28 20:25:31 +0700

    implemented pokemon-search.test for more precision :muscle:

--- src/__test__/pokemon-search.test.tsx
+++ src/__test__/pokemon-search.test.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { act, render, screen } from '@testing-library/react';
+import { act, render, screen,waitFor,cleanup  } from '@testing-library/react';
 import App from '../App';
 import {BrowserRouter} from 'react-router-dom'
-
-const SAFE_TIMEOUT = 2 * 1000 // 2 seconds [could be more read @EXPLANATION]
 
 const renderWithRouter = (ui:any, {route = '/'} = {}) => {
   window.history.pushState({}, 'Test page', route)
@@ -14,15 +12,19 @@ const renderWithRouter = (ui:any, {route = '/'} = {}) => {
 /**
  * @NOTE No need to manually enter the text in the input to be testing because I already made that logic applied normally when recieved the query string
  * @BY THITI-DEV
- * @EXPLANATION We first then render the whole app into the scope then navigating to specific route and then wait for 2 seconds in order to wait for the response from graphql
- * @RISK note that 2 seconds might not be safe it could be any longer because if the deployed service has gone sleep [it may should more than 2 seconds]
+ * @EXPLANATION We first then render the whole app into the scope then navigating to specific route and then wait for the graphql return the result by using waitFor
+ * @RISK If the hosted-server(graphql) just wake up from the sleep state [the test process might take longer]
  */
+
+//afterEach(cleanup)
+beforeEach(cleanup)
+
 
 test('Searching the Bulbasaur by query string', async() => {
   const route = '/pokemon-search?search=Bulbasaur'
-  await act(async() => {
+  await act(async () => {
     renderWithRouter(<App />, {route})
-    await new Promise((r) => setTimeout(r, SAFE_TIMEOUT));
+    await waitFor(() => screen.getByTestId("pokemon-type"))
   })
   expect(screen.getByText(/Types:Grass,Poison/i)).toBeInTheDocument()
 })
@@ -31,7 +33,7 @@ test('Searching the Charmander by query string', async() => {
   const route = '/pokemon-search?search=Charmander'
   await act(async() => {
     renderWithRouter(<App />, {route})
-    await new Promise((r) => setTimeout(r, SAFE_TIMEOUT));
+    await waitFor(() => screen.getByTestId("pokemon-type"))
   })
   expect(screen.getByText(/Types:Fire/i)).toBeInTheDocument()
 })
@@ -40,7 +42,7 @@ test('Searching the Squirtle by query string', async() => {
   const route = '/pokemon-search?search=Squirtle'
   await act(async() => {
     renderWithRouter(<App />, {route})
-    await new Promise((r) => setTimeout(r, SAFE_TIMEOUT));
+    await waitFor(() => screen.getByTestId("pokemon-type"))
   })
   expect(screen.getByText(/Types:Water/i)).toBeInTheDocument()
 })
