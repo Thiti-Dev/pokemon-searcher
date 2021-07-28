983a56ffd4f6ac7a30452b25a8bbf93f6497d007
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-28 12:18:20 +0700

    core->apollo->wrapper->pokemon: made generic of the returned pokemon from gql unioned with null

--- src/core/apollo/queries/wrappers/pokemon.wrapper.ts
+++ src/core/apollo/queries/wrappers/pokemon.wrapper.ts
@@ -2,5 +2,5 @@ import { useQuery } from '@apollo/client';
 import { IPokemon } from '../../../../shared/interfaces/pokemon.interface';
 import { QUERY_GET_POKEMON } from '../constants/GET_POKEMON';
 export function $HOOK_GetPokemonQuery(name: string) {
-    return useQuery<{ pokemon: IPokemon }>(QUERY_GET_POKEMON, { variables: { pokemon_name: name } });
+    return useQuery<{ pokemon: IPokemon | null }>(QUERY_GET_POKEMON, { variables: { pokemon_name: name } });
 }
